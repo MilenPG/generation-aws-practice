70b3ce0bb6f6e0827f4d9cbfe03e3f60c34edccc
Milen P G <153119973+MilenPG@users.noreply.github.com>
2025-11-24 16:39:27 +0000

    Python labs / Working with numeric data types

---
+++ numeros_interactivos.py
@@ -0,0 +1,12 @@
+numero1 = float(input("Ingresa el primer número: ")) # casting: conversión entre tipos de datos
+numero2 = float(input("Ingresa el segundo número: "))
+
+# print("Suma: ",int((number1)+(number2)))
+# print("Resta: ", (number1)-(number2)) 
+
+print(f"La suma de {numero1} y {numero2} es {numero1 + numero2}")
+print(f"La resta de {numero1} y {numero2} es {numero1 - numero2}")
+print(f"La multiplicación de {numero1} y {numero2} es {numero1 * numero2}")
+print(f"La división de {numero1} y {numero2} es {numero1 / numero2}")
+print(f"El módulo de {numero1} y {numero2} es {numero1 % numero2}")
+print(f"{numero1} elevado a la potencia de {numero2} es {numero1 ** numero2}")
